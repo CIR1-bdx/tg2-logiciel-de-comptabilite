ef05ddd4d92cfbaa4d0d46a697f78f68efd35324
agathemczk <agathe.maciaszczyk@icloud.com>
2023-09-15 12:14:52 +0200

    commit

--- calcul montant imposable .c
+++ calcul montant imposable .c
@@ -1,6 +1,7 @@
 # include <stdio.h>
 #include ''
 
+<<<<<<< Updated upstream
 int taxable_amount (int sales) {
     printf("Quel est votre chiffre d'affaires? :\n");
     scanf("%d",&sales);
@@ -8,3 +9,14 @@ int taxable_amount (int sales) {
     return taxableamount;
     printf("Votre montant imposable est de %d euros.\n",taxableamount);
 }
+=======
+int categorie_L = 0;
+int categorie_V = 1;
+
+int montant_imposable (int chiffre_affaires) {
+    printf("Quel est votre chiffre d'affaires? :\n");
+    scanf("%d",&chiffre_affaires);
+    int montant_imp = chiffre_affaires - (categorie_L + categorie_V);
+    printf("Votre montant imposable est de %d euros.\n",montant_imp);
+}
+>>>>>>> Stashed changes
