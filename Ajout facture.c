17fa0a0b35df8bcb7b6325a01b50d0bea91715c0
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2023-09-15 12:34:16 +0200

    Revert "Merge branch 'main' of https://github.com/CIR1-bdx/tg2-logiciel-de-comptabilite"
    This reverts commit 4907cea1272d3fe657e92014bc7971977d3e21d5, reversing
    changes made to 6996d28d7b4d8cf895eabe8858bc1c1bdb5d08b9.

--- Ajout facture.c
+++ Ajout facture.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 
-int ajout_facture (char montant, char categorie, char type, char day, char month, char liste[]){
+int ajout_facture (char montant, char categorie, char date, char liste[]){
     if (recherche_place() >= 0){
-        liste_Facture[0].prixHT = montant;
-        liste_Facture[1].categorie = categorie;
-        liste_Facture[2].day = day;
-        liste_Facture[3].month = month;
-        liste_Facture[4].ID = recherche_place();
-        liste_Facture[5].type = type
-        liste[recherche_place()] = liste_Facture[] ;
+        T[0].prixHT = montant;
+        T[1].prixTTC =
+        T[2] = categorie;
+        T[3] = date;
+        T[4] = recherche_place();
+        liste[recherche_place()] = T[] ;
         return 1;
 
     }else{
--- calcul montant imposable .c
+++ calcul montant imposable .c
@@ -1,13 +1,9 @@
 # include <stdio.h>
-#include "Ajout facture.c"
-#include "structure.c"
-
-struct Factures;
 
 int montant_imposable (int chiffre_affaires) {
     printf("Quel est votre chiffre d'affaires? :\n");
     scanf("%d",&chiffre_affaires);
-    int montant_imp = chiffre_affaires - (categorieV + categorieL);
+    int montant_imp = chiffre_affaires - (categorie_L + categorie_V);
     return montant_imp;
     printf("Votre montant imposable est de %d euros.\n",montant_imp);
 }
