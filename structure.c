7f84707e0f9839ce6ace3a4dad67be97ab59c235
Charles-Antoine <charto.bta@gmail.com>
2023-09-15 12:06:24 +0200

    struct

--- structure.c
+++ structure.c
@@ -7,16 +7,3 @@ struct Factures{
 };
 
 struct Factures T[3];
-
-int initialisation() {
-    for (int i = 0; i < 3; ++i){
-        struct Factures factureTemporaire;
-        factureTemporaire.ID;
-        factureTemporaire.prixHT;
-        factureTemporaire.prixTTC;
-        factureTemporaire.date;
-        factureTemporaire.categorie;
-        T[i] = factureTemporaire;
-    }
-    return 0;
-}
