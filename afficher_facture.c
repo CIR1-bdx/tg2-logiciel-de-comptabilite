8068f02a7692d3376962258bde619bdc41af038d
Charles-Antoine <charto.bta@gmail.com>
2023-09-21 15:48:12 +0200

    v

--- afficher_facture.c
+++ afficher_facture.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
-#include "structure.c"
+#include "search_function.c"
 
 void recuperation_facture(int factureID){
-    for (int i = 0; i < 3; ++i) {
-        if (liste_Facture[i].ID == factureID){
-            printf()
+    int place = searchFacture(factureID);
+    if (place != 1){
+        if (liste_Facture[place].type == 0){
+            printf("|Credit | ID: %d | PrixHT: %d | PrixTTC: %d | Date: %d/%d | Categorie: %c |",liste_Facture[place].ID,liste_Facture[place].prixHT,liste_Facture[place].prixTTC,liste_Facture[place].day,liste_Facture[place].month,liste_Facture[place].categorie);
+        }
+        else{
+            printf("|Debit | ID: %d | PrixHT: %d | PrixTTC: %d | Date: %d/%d | Categorie: %c |",liste_Facture[place].ID,liste_Facture[place].prixHT,liste_Facture[place].prixTTC,liste_Facture[place].day,liste_Facture[place].month,liste_Facture[place].categorie);
         }
     }
+    return;
 }
