cfc30a3fbf5c91c21a7e84e46becd1a9c346eb6f
Charles-Antoine <charto.bta@gmail.com>
2023-09-21 14:36:33 +0200

    v

--- Calcul montant gagné ou perdue.c
+++ Calcul montant gagné ou perdue.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include "structure.c"
-int CalculTotalJoursMois(int JourMoisRechercher){
-    int JourMoisRechecher;
-    int somme;
-    for (int i = 0; i < 10; i++){
-        if (JourMoisRechecher ==liste_Facture[i]){
-            if (liste_Facture[i].prixHT == 0) {
-                return  somme=liste_Facture[i].prixHT;
-            } else {
-                return 0;
-            }
-            if (JourMoisRechecher=liste_Facture[i].prixTTC == 1){
-                return somme=liste_Facture[i].prixTTC;
-            }else{
-                return 0;
-            }
-        } else {
-            return 0;
+
+int Calcule_jour_mois(int daysearch, int monthsearch){
+    int sommeGagne = 0;
+    int sommePerdu = 0;
+    for (int i = 0; i < 10; ++i) {
+        if(liste_Facture[i].day == daysearch && liste_Facture[i].month == monthsearch){
+            if (liste_Facture[i].type == 0) sommeGagne += liste_Facture[i].prixTTC;
+            else if (liste_Facture[i].type == 1) sommePerdu += liste_Facture[i].prixTTC;
+        }else if(liste_Facture[i].day == 0 && liste_Facture[i].month == monthsearch) {
+            if (liste_Facture[i].type == 0) sommeGagne += liste_Facture[i].prixTTC;
+            else if (liste_Facture[i].type == 1) sommePerdu += liste_Facture[i].prixTTC;
         }
     }
- return 0;
+    return sommeGagne - sommePerdu;
 }
+
